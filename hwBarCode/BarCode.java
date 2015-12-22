8787f7d4169584444adaf2e461bda0a01a7e7a62
Matthew Zhu <mzhu3@stuy.edu>
2015-12-22 12:16:45 -0500

    Fixed ComparedTo

--- hwBarCode/BarCode.java
+++ hwBarCode/BarCode.java
@@ -3,6 +3,8 @@ public class BarCode implements Comparable{
     private String _zip;
     private int _checkDigit;
     public static String[] conversions;
+    private String Barcode;
+
 
     // constructors
     //precondtion: zip.length() = 5 and zip contains only digits.
@@ -13,11 +15,20 @@ public class BarCode implements Comparable{
 	_zip = zip;
 	_checkDigit = checkSum();
 	conversions =new String[]{"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::"};
+	Barcode = Convert(_zip);
+	
 
     }
 
     // postcondition: Creates a copy of a bar code.
-    public BarCode(BarCode x){}
+    public BarCode(BarCode x){
+	conversions =new String[]{"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::"};
+	_zip = ConvertToZip(x.Barcode);
+	_checkDigit = checkSum();
+	Barcode = Convert(_zip);
+	
+
+    }
 
 
     //post: computes and returns the check sum for _zip
@@ -30,21 +41,37 @@ public class BarCode implements Comparable{
 	sum += Integer.parseInt(_zip.substring(_zip.length()-1));
 	return sum%10;
     }
-    public String Convert(){
+    public String Convert(String zips){
 	String ans="|" ;
-	for (int i = 0; i < _zip.length() - 1; i ++){
-	    ans += conversions[Integer.parseInt(_zip.substring(i, i + 1))];
+	for (int i = 0; i < zips.length() - 1; i ++){
+	    ans += conversions[Integer.parseInt(zips.substring(i, i + 1))];
 	}
-	ans += conversions[Integer.parseInt(_zip.substring(_zip.length()-1))];
+	ans += conversions[Integer.parseInt(zips.substring(zips.length()-1))];
 	ans += "|";
 	return ans;
 	
 	    
     }
+    public String ConvertToZip(String Barcodes){
+	String ans = Barcodes.substring(1,Barcodes.length()-1);
+	String zipper = "";
+	for(int i = 0; i < ans.length() - 6; i++){
+	    for(int j = 0; j < conversions.length; j++){
+		if(ans.substring(i,i + 6).equals(conversions[j])){
+		    zipper+=j;
+		}
+	    }
+	}
+	return zipper;
+    }
+
+				       
+
+
     //postcondition: format zip + check digit + barcode 
     //ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|" 
     public String toString(){
-	return _zip + _checkDigit +" " + Convert();
+	return _zip + _checkDigit +" " + Convert(_zip);
     }
 
 
@@ -59,13 +86,20 @@ public class BarCode implements Comparable{
 
 
     public int compareTo(Object other){
-	return 1;
+	if( toString(). equals((((BarCode)other).toString()))){
+	    return 0;
+	}
+	else{
+	    return -1;
+	}
     }
     // postcondition: compares the zip + checkdigit
     public static void main(String[] args){
 	BarCode b1 = new BarCode("12345");
+	BarCode b2 = new BarCode("12331");
 	System.out.println(b1.toString());
 	//System.out.println(b1._checkDigit);
+	System.out.println(b1.compareTo(b2));
     }
 
 }
